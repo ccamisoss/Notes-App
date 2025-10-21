e426e569ce82c6df0967014f0f85583a17341840
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2025-10-21 20:31:35 -0300

    Feat: Integrate delete, edit and archive functionality in Note component with user notifications using SweetAlert2

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,8 +1,71 @@
-export default function Note({ title, children }) {
+import Swal from "sweetalert2";
+import { deleteNote, toggleArchive } from "../services/NoteService";
+import NoteMenu from "./NoteMenu";
+import Tag from "./Tag";
+import { useNavigate } from "react-router-dom";
+
+export default function Note({ note, refresh }) {
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    try {
+      const res = await deleteNote(note.id);
+
+      if (res.error) throw new Error(res.error);
+
+      Swal.fire({
+        icon: "success",
+        text: res.message,
+      });
+
+      refresh();
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        text: error.message,
+      });
+    }
+  };
+
+  const handleEdit = () => {
+    navigate(`/edit/${note.id}`);
+  };
+
+  const handleToggleArchive = async () => {
+    try {
+      const res = await toggleArchive(note.id, !note.archived);
+
+      if (res.error) throw new Error(res.error);
+
+      Swal.fire({
+        icon: "success",
+        text: res.message,
+      });
+
+      refresh();
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        text: error.message,
+      });
+    }
+  };
+
   return (
-    <div className="h-32 p-4 bg-white rounded-lg shadow space-y-2">
-      {title && <h3 className="font-semibold text-lg">{title}</h3>}
-      {children}
+    <div className="relative flex flex-col min-h-32 px-4 py-2.5 bg-white rounded-lg shadow space-y-2">
+      <NoteMenu
+        onDelete={handleDelete}
+        onEdit={handleEdit}
+        isArchived={note.archived}
+        onToggleArchive={handleToggleArchive}
+      />
+      <h3 className="font-semibold text-lg">{note.title}</h3>
+      <p className="flex-1">{note.content}</p>
+      <div className="flex flex-wrap gap-1">
+        {note.NoteTag.map((tag) => (
+          <Tag key={tag.id} name={tag.name} />
+        ))}
+      </div>
     </div>
   );
 }
