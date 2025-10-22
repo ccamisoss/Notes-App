73e217895f6b72fd14805fa2e9f511a5615a7836
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2025-10-22 00:22:08 -0300

    Enhance: Update favicon to use custom SVG icons and add new black and white note icons for improved visual consistency in the app

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import noteIcon from "../assets/icons/note-black.svg";
+
 export function Header() {
   return (
-    <header className="space-y-4 p-4 bg-white border shadow">
-      <h1 className="text-2xl font-bold">Notes App</h1>
+    <header className="p-4 bg-white flex items-baseline gap-2 border shadow">
+      <img src={noteIcon} className="size-7" />
+      <h1 className="text-3xl font-bold">Notes App</h1>
     </header>
   );
 }
