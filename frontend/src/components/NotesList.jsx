3641727ade4294939a018a9d2451463a0dc8be0f
Camila Lucía Sosa <camilaluciasosa@hotmail.com>
2025-10-21 21:52:34 -0300

    Feat: Implement tag filtering in NotesList and enhance Sidebar with dynamic tag links for improved note organization and navigation

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import Note from "./Note";
+import { useSearchParams } from "react-router-dom";
 
 const NotesList = ({ notes, refresh }) => {
-  if (!notes[0]) {
+  const [searchParams] = useSearchParams();
+  const tag = searchParams.get('tag');
+  console.log(tag)
+
+  const filteredNotes = tag
+    ? notes.filter(note => 
+        note.NoteTag.some(noteTag => noteTag.name === tag)
+      )
+    : notes;
+
+  if (!filteredNotes[0]) {
     return (
       <div className="justify-center w-full h-full flex-1 flex flex-col text-center gap-8 text-gray-500 font-semibold">
         <p className="text-8xl">;(</p>
@@ -13,7 +24,7 @@ const NotesList = ({ notes, refresh }) => {
 
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-      {notes.map((note) => (
+      {filteredNotes.map((note) => (
         <Note refresh={refresh} note={note} key={note.id} />
       ))}
     </div>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { getTags } from "../services/TagService";
 
 export function Sidebar() {
   const location = useLocation();
+  const [tags, setTags] = useState([]);
+
+  const fetchTags = async () => {
+    const res = await getTags();
+
+    if (!res.error) {
+      setTags(res);
+    }
+  };
+
+  useEffect(() => {
+    fetchTags();
+  }, []);
 
   return (
     <div className="flex flex-col gap-4 p-3 w-full sm:w-1/6 bg-white shadow">
@@ -18,7 +33,7 @@ export function Sidebar() {
       <Link
         to="/"
         className={`px-3 py-2 rounded transition-colors ${
-          location.pathname === "/"
+          location.pathname === "/" && !location.search
             ? "bg-indigo-100 font-semibold"
             : "text-gray-700 hover:bg-gray-100"
         }`}
@@ -35,6 +50,25 @@ export function Sidebar() {
       >
         Archived
       </Link>
+      {tags[0] && (
+        <div className="flex flex-col gap-2">
+          <label className="border-y py-2 text-center font-semibold">
+            Tags
+          </label>
+          {tags.map((tag) => (
+            <Link
+              to={`/?tag=${tag.name}`}
+              className={`px-3 py-2 rounded transition-colors ${
+                location.search === `?tag=${tag.name}`
+                  ? "bg-indigo-100 font-semibold"
+                  : "text-gray-700 hover:bg-gray-100"
+              }`}
+            >
+              {tag.name}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
